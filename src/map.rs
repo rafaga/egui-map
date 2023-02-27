201ccb514f1ad9b79e3d3d29fad183c5bcaf952e
Rafael Amador Galván <rafael.amador@gmail.com>
2023-02-26 22:56:02 -0600

    testing positioning

--- src/map.rs
+++ src/map.rs
@@ -49,7 +49,7 @@ impl Widget for Map {
             }
             else {
                 let font_id = FontId{size:20.0, family:FontFamily::Proportional};
-                ui_obj.painter().text( Pos2{x:12.0f32, y:12.0f32 },Align2::CENTER_CENTER, "loading", font_id, Color32::LIGHT_RED);
+                ui_obj.painter().text( Pos2{x:12.0f32, y:12.0f32 },Align2::LEFT_TOP, "loading", font_id, Color32::LIGHT_RED);
             }
             
             /*
